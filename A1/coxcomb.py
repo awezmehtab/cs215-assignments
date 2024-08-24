8b4b71beae49c1293f0d172bc79c18d9bd80df5e
awezmehtab <awezmehtab@users.noreply.github.com>
2024-08-24 19:39:39 +0530

    q7 almost done

---
+++ A1/coxcomb.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load data
+data = pd.read_csv("./data/Restaurant-Complaints")
+data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
+complaints = data["complaint"]
+counts = data["count"]
+
+n = len(complaints)
+angles = np.linspace(0, 2 * np.pi, n, endpoint=False).tolist()
+angles += angles[:1]
+
+counts = counts.tolist()
+counts += counts[:1]
+
+colors = plt.cm.viridis(np.linspace(0, 1, n))
+
+fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+
+for i in range(n):
+    ax.bar(
+        x=angles[i],
+        height=counts[i],
+        width=2 * np.pi / n,
+        color=colors[i],
+        edgecolor="black",
+        align="edge",
+    )
+
+ax.set_xticks(angles[:-1])
+ax.set_xticklabels(complaints)
+
+ax.set_yticklabels([])
+ax.yaxis.grid(False)
+ax.spines["polar"].set_visible(False)
+plt.title("Complaints from customers in a restaurant")
+plt.savefig("./images/coxcomb.png")
+plt.show()
--- A1/pareto-plot.py
+++ A1/pareto-plot.py
@@ -1,8 +1,30 @@
-import numpy as np
 import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("./data/bar_locations.csv")
+complaint_data = pd.read_csv("./data/Restaurant-Complaints")
+complaint_data = complaint_data.sort_values(by="count", ascending=False)
+complaint_data["Cumulative Percentage"] = (
+    complaint_data["count"].cumsum() / complaint_data["count"].sum() * 100
+)
+fig, ax1 = plt.subplots()
 
-plt.figure(figsize=(10,6))
+ax1.bar(complaint_data["complaint"], complaint_data["count"], color="C0")
+ax1.set_xlabel("complaint")
+ax1.set_ylabel("count", color="C0")
+ax1.tick_params(axis="y", labelcolor="C0")
 
+ax2 = ax1.twinx()
+ax2.plot(
+    complaint_data["complaint"],
+    complaint_data["Cumulative Percentage"],
+    color="C1",
+    marker="o",
+)
+ax2.set_ylabel("cumulative %", color="C1")
+ax2.tick_params(axis="y", labelcolor="C1")
+
+plt.xticks(rotation=45, ha="right")
+plt.title("Complaints by customers of a restaurant")
+plt.tight_layout()
+plt.savefig("./images/pareto-plot.png")
+plt.show()
