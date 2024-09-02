42ed0f44d36b30eb1c21c6c8b11c41bc0ba08a3f
vaibhavDoesntCode <23b1068@iitb.ac.in>
2024-09-02 20:50:46 +0530

    2C done

--- A2/code/2c.py
+++ A2/code/2c.py
@@ -5,9 +5,9 @@
 
 # Importing the libraries
 import numpy as np
-from scipy.stats import norm 
+from scipy.stats import norm, gaussian_kde
 import matplotlib.pyplot as plt
-import seaborn as sb
+
 
 N = int(1e5)
 
@@ -16,24 +16,23 @@ def sample(loc, scale):
     F_X_inverse = norm.ppf(x, loc=loc, scale=scale)
     return F_X_inverse
 
-# Define the four parameter choices for (mean, variance)
+
 params = [(0, 0.2), (0, 1.0), (0, 5.0), (-2, 0.5)]
 samples = [sample(mu, sigma) for mu, sigma in params]
 
-# Step 4: Plot the results
+
 x = np.linspace(-5, 5, 1000)
 plt.figure(figsize=(10, 6))
 
-for (mu, sigma), samplei in zip(params, samples):
-    n,x,_ = plt.hist(samplei,1000,histtype=u'step')
-    bin_centers = 0.5*(x[1:]+x[:-1])
-    sb.distplot(x, hist=False)
-    # plt.plot(bin_centers,n, label=f'μ={mu}, σ={sigma}')
+for (mu, sigma), samplei in zip(params, samples):    
+    kde = gaussian_kde(samplei, bw_method=0.1)
+    plt.plot(x, kde(x), label=f'μ={mu}, σ={sigma}')
 
 plt.title('Probability Density Functions of Gaussians')
 plt.xlabel('X')
 plt.ylabel('φ(x)')
 plt.legend()
 plt.grid(True)
+plt.savefig('../images/2C.png')
 plt.show()
 
